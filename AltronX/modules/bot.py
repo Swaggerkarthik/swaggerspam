affdc73dc065703534a24f34cde6cba36e5f589a
Swaggerkarthik <136192288+Swaggerkarthik@users.noreply.github.com>
2023-07-17 16:08:36 +0530

    Update bot.py

--- AltronX/modules/bot.py
+++ AltronX/modules/bot.py
@@ -20,10 +20,10 @@ from telethon import events
 async def ping(e):
     if e.sender_id in SUDO_USERS:
         start = datetime.now()
-        altron = await e.reply(f"» #​🇸​​🇭​​🇮​​🇻​​🇦​​🇲​_ᴏᴘ_ʙᴏʟᴛᴀ", parse_mode=None, link_preview=None)
+        altron = await e.reply(f"» #​𝙎𝙒𝘼𝙂𝙂𝙀𝙍​_ᴏᴘ_ʙᴏʟᴛᴀ", parse_mode=None, link_preview=None)
         end = datetime.now()
         mp = (end - start).microseconds / 1000
-        await altron.edit(f"__🤖 ᴘɪɴɢ нαтєяѕ мєяє ℓαυяє ρє​__\n» `{mp} ms`")
+        await altron.edit(f"__🤖 ᴘɪɴɢ ʜᴀᴛᴇʀꜱ ᴍᴇ 52 ɢᴀᴊ ᴋɪ ʟᴜɴᴅ​__\n» `{mp} ms`")
 
 
 @MK1.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
